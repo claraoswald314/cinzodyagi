23c8c825a14aec82c4b56bf24b4fd11df19af37e
claraoswald314 <86008186+claraoswald314@users.noreply.github.com>
2023-01-18 13:46:22 +0300

    patika.dev in java 101 derslerinin çin zodyağı hesaplama ödevidir

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        double yil;
+        String zodyag;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("doğduğunuz yılı yazınız : ");
+        yil = scanner.nextDouble();
+        yil = yil%12;
+
+        switch((int) yil){
+            case 0: System.out.println("çin zodyağınız maymundur");
+                break;
+            case 1: System.out.println("çin zodyağınız horozdur");
+                break;
+            case 2: System.out.println("çin zodyağınız köpektir");
+                break;
+            case 3: System.out.println("çin zodyağınız domuzdur");
+                break;
+            case 4: System.out.println("çin zodyağınız faredir");
+                break;
+            case 5: System.out.println("çin zodyağınız öküzdür");
+                break;
+            case 6: System.out.println("çin zodyağınız kaplandır");
+                break;
+            case 7: System.out.println("çin zodyağınız tavşandır");
+                break;
+            case 8: System.out.println("çin zodyağınız ejderhadır");
+                break;
+            case 9: System.out.println("çin zodyağınız yılandır");
+                break;
+            case 10: System.out.println("çin zodyağınız attır");
+                break;
+            case 11: System.out.println("çin zodyağınız koyundur");
+                break;
+            default: System.out.println("yanlış değer girdiniz");
+
+
+
+        }
+
+
+        
+
+
+
+
+
+
+
+    }
+
+}
